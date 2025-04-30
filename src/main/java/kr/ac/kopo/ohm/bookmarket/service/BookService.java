6daabb8fca8299829ddb3fb923d71e91ee4707db
hyunmin-ooo <2201110357@office.kopo.ac.kr>
2025-04-30 09:58:28 +0900

    BookMarket의 5장 카테고리와 출판사명이 필터링된 검색목록

--- src/main/java/kr/ac/kopo/ohm/bookmarket/service/BookService.java
+++ src/main/java/kr/ac/kopo/ohm/bookmarket/service/BookService.java
@@ -3,9 +3,13 @@ package kr.ac.kopo.ohm.bookmarket.service;
 import kr.ac.kopo.ohm.bookmarket.domain.Book;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface BookService {
     List<Book> getAllBookList();
     Book getBookById(String bookId);
     List<Book>getAllBookListByCategory(String category);
+    Set<Book> getBookListByFilter(Map<String, List<String>> filter);
+
 }
